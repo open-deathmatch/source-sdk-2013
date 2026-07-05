bdfd19ac97242ba76643b14d8739d54b9435d5c4
ThePixelMoon <yourlocalmoon7@gmail.com>
2026-07-05 11:16:26 +0300

    fix: fix broken sprays for client indices greater than 64

--- src/game/server/te_playerdecal.cpp
+++ src/game/server/te_playerdecal.cpp
@@ -94,7 +94,11 @@ void CTEPlayerDecal::Test( const Vector& current_origin, const QAngle& current_a
 IMPLEMENT_SERVERCLASS_ST(CTEPlayerDecal, DT_TEPlayerDecal)
 	SendPropVector( SENDINFO(m_vecOrigin), -1, SPROP_COORD),
 	SendPropInt( SENDINFO(m_nEntity), MAX_EDICT_BITS, SPROP_UNSIGNED ),
+#ifndef DEATHMATCH
 	SendPropInt( SENDINFO(m_nPlayer), Q_log2( MAX_PLAYERS ), SPROP_UNSIGNED ),
+#else
+	SendPropInt( SENDINFO( m_nPlayer ), Q_log2( MAX_PLAYERS ) + 1, SPROP_UNSIGNED ),
+#endif
 END_SEND_TABLE()
 
 
